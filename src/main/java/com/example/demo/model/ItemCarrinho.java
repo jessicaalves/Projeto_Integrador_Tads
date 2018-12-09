426f93a03093487f8372bde076626d2eae2eb09a
jessica <jessicaalvesferreira24@gmail.com>
2018-12-09 19:43:00 -0200

    Versão 2.0.

--- src/main/java/com/example/demo/model/ItemCarrinho.java
+++ src/main/java/com/example/demo/model/ItemCarrinho.java
@@ -6,27 +6,20 @@
 package com.example.demo.model;
 
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-
 
 /**
  *
  * @author jessica
  */
 @Entity
-public class Carrinho {
-    private Long id;
-    private List<Produto> produtos;
-    private List<ItemCarrinho> itens;
+public class ItemCarrinho {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private List<Produto>produtos;
+    private Long quantidade;
+    private List<Carrinho>carrinho;
+
     public Long getId() {
         return id;
     }
@@ -35,14 +28,28 @@ public class Carrinho {
         this.id = id;
     }
 
-    @OneToMany(cascade={CascadeType.PERSIST,CascadeType.MERGE,CascadeType.REMOVE})
-   // @JoinColumn(name = "id_Carrinho")
     public List<Produto> getProdutos() {
         return produtos;
     }
 
     public void setProdutos(List<Produto> produtos) {
         this.produtos = produtos;
+    }
+
+    public Long getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Long quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public List<Carrinho> getCarrinho() {
+        return carrinho;
+    }
+
+    public void setCarrinho(List<Carrinho> carrinho) {
+        this.carrinho = carrinho;
     }
     
 }
---
+++ src/main/java/com/example/demo/model/ItemVenda.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.model;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author jessica
+ */
+@Service
+public class ItemVenda {
+    
+    private Long id;
+    private double custo;
+    private List<Produto>produtos;
+    
+}
